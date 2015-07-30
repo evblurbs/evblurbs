42af89bccd998cd3e74e6f3a3f8f2ec5f6827ff6
Evan Johnson <evan@createinform.com>
2015-07-29 21:17:56 -0700

    Update server file to use submodule.

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ var connect = require('connect');
 var http = require('http');
 var vhost = require('vhost');
 var serverStatic = require('serve-static');
+var compression = require('compression');
 
 // app for evblurbs.io
 var mainapp = connect();
 // app for fewd
 var fewdapp = connect();
 
-var compression = require('compression');
-//mainapp.use(compression());
-//fewdapp.use(compression());
-
 // set static directories
 mainapp.use(serverStatic(__dirname + "/dist"));
-fewdapp.use(serverStatic(__dirname + "/fewdapp"));
+fewdapp.use(serverStatic(__dirname + "/sea-fewd/dist"));
 
 // app for all apps
 var app = connect();
@@ -22,5 +19,5 @@ app.use(compression());
 app.use(vhost('seafewd5.evblurbs.io', fewdapp));
 app.use(vhost('www.evblurbs.io', mainapp));
 app.use(vhost('evblurbs.io', mainapp));
-//app.use(serverStatic(__dirname + '/dist'));
+
 http.createServer(app).listen(process.env.PORT || 80);

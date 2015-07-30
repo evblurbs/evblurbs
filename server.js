28098b2011f8ba33a7de72fdfbdb987f64f2c854
Evan Johnson <evan@createinform.com>
2015-07-29 19:52:15 -0700

    Try ans setup subdomain.

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 var connect = require('connect');
 var http = require('http');
-var app = connect();
+var vhost = require('vhost');
 
-var compression = require('compression')
-app.use(compression())
+// app for evblurbs.io
+var mainapp = connect();
+// app for fewd
+var fewdapp = connect();
 
+var compression = require('compression');
+mainapp.use(compression());
+fewdapp.use(compression());
+
+// set static directories
 var static = require('serve-static');
-app.use(static(__dirname + "/dist"));
+mainapp.use(static(__dirname + "/dist"));
+fewdapp.use(static(__dirname + "/fewdapp"));
+
+// app for all apps
+var app = connect();
+app.use(vhost('seafewd5.evblurbs.io', fewdapp));
+app.use(vhost('www.evblurbs.io', mainapp));
+app.use(vhost('evblurbs.io', mainapp));
 
 http.createServer(app).listen(process.env.PORT || 80);
--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var config = {
     this.resolve.alias[name] = path;
     this.module.noParse.push(new RegExp('^' + name + '$'));
   },
-  
+
   // We split the entry into two specific chunks. Our app and vendors. Vendors
   // specify that react should be part of that chunk
   entry: {
@@ -15,7 +15,7 @@ var config = {
     vendors: ['react']
   },
   resolve: { alias: {} },
-  
+
   // We add a plugin called CommonsChunkPlugin that will take the vendors chunk
   // and create a vendors.js file. As you can see the first argument matches the key
   // of the entry, "vendors"

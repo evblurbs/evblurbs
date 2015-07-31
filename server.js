307b913b34cb368ad2030abdc4ec723359c68ebd
Evan Johnson <evan@createinform.com>
2015-07-30 23:28:56 -0700

    Print out access token.

--- server.js
+++ server.js
@@ -47,6 +47,6 @@ githubOAuth.on('error', function(err) {
 
 githubOAuth.on('token', function(token, serverResponse) {
   console.log('here is your shiny new github oauth token', token);
-  serverResponse.end(githubUtils.getUserInfo());
+  serverResponse.end(JSON.stringify(token));
 });
 

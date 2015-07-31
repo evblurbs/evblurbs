414a4d8367e15b8dec98e12a1fd6d81c0f9e1d1a
Evan Johnson <evan@createinform.com>
2015-07-31 01:25:29 -0700

    Pass in token.

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ githubOAuth.on('token', function(token, serverResponse) {
 
   var options = {
     hostname: 'api.github.com',
-    path: '/user?access_token=046be40da20b63d5f784be834c55ed670f0b0208',
+    path: '/user?access_token=' + token.access_token,
     method: 'GET',
     headers: {
       'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13'

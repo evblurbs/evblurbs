6f7f32cbbd07d1d7540f1199d9fad564724bf8bd
Evan Johnson <evajohns@amazon.com>
2015-07-30 19:04:29 -0700

    Update server file.

--- server.js
+++ server.js
@@ -20,4 +20,32 @@ app.use(vhost('seafewd5.evblurbs.io', fewdapp));
 app.use(vhost('www.evblurbs.io', mainapp));
 app.use(vhost('evblurbs.io', mainapp));
 
-http.createServer(app).listen(process.env.PORT || 80);
+var githubOAuth = require('github-oauth')({
+  githubClient: process.env['GITHUB_CLIENT'],
+  githubSecret: process.env['GITHUB_SECRET'],
+  baseURL: 'http://localhost',
+  loginURI: '/login',
+  callbackURI: '/callback',
+  scope: 'user' // optional, default scope is set to user 
+});
+
+require('http').createServer(function(req, res) {
+  if (req.url.match(/login/)) {
+    return githubOAuth.login(req, res)
+  }
+  else if (req.url.match(/callback/)) {
+    return githubOAuth.callback(req, res)
+  } else {
+    return app
+  }
+}).listen(process.env.PORT || 80);
+
+githubOAuth.on('error', function(err) {
+  console.error('there was a login error', err)
+});
+
+githubOAuth.on('token', function(token, serverResponse) {
+  console.log('here is your shiny new github oauth token', token)
+  serverResponse.end(JSON.stringify(token))
+});
+
